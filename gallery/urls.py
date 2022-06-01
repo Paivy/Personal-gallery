c80ac2d69bfbf7094f99fc87cb61267e58d19020
Paivy <paivyeshirera@gmail.com>
2022-06-01 06:24:03 +0300

    added the navbar

--- gallery/urls.py
+++ gallery/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('photos/', include('photos.urls')),
+    path('', include('photos.urls')),
     path('admin/', admin.site.urls),
    
 ]
