ccc5e1c717515e70252d0ab6fc3b7d0a7d31f0d7
Paivy <paivyeshirera@gmail.com>
2022-06-02 14:15:03 +0300

    added more images to the site

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.posted_pics, name='Home'),
-    path('big_image/<pic_id>', views.big_image, name="big_image"),
+    path('big_image/<pic_id>', views.big_image, name="bigimage"),
     path('search/', views.search_results, name='search_results')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
