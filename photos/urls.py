88d72353ef68b67300976e90d616fc9027254cd1
Paivy <paivyeshirera@gmail.com>
2022-06-02 04:21:13 +0300

    added path to be able to load page

--- photos/urls.py
+++ photos/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name ='home'),
-    path('gallery/',views.gallery, name ='gallery'),
-    path('photo/<str:pk>',views.viewPhoto, name ='photo'),
-    path('add/',views.addPhoto, name ='add'),
-
+    path('',views.home, name='home'),
+    path('home/',views.home, name='home'),
+    path('show/', views.show, name='show'),
+    path('search/', views.search, name='searchResults'),
+    re_path('^location/(?P<locale>\w+)/', views.location, name='location'),
 ]
+
